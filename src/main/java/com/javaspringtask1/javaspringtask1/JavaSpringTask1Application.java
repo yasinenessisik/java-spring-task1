01acdad41ec2e2797b08fa9e704ff444f65018f2
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-03-30 18:06:27 +0300

    Initial commit

---
+++ src/main/java/com/javaspringtask1/javaspringtask1/JavaSpringTask1Application.java
@@ -0,0 +1,13 @@
+package com.javaspringtask1.javaspringtask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaspringtask1/javaspringtask1/JavaSpringTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringtask1.javaspringtask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
