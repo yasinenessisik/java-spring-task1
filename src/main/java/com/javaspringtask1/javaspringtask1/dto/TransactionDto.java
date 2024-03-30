ff46b6ae2cfe36fddb465b3e99212a1b13299650
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-03-30 18:56:06 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/javaspringtask1/javaspringtask1/dto/TransactionDto.java
+++ src/main/java/com/javaspringtask1/javaspringtask1/dto/TransactionDto.java
@@ -1,9 +1,7 @@
 package com.javaspringtask1.javaspringtask1.dto;
 
 import com.javaspringtask1.javaspringtask1.model.Account;
-import com.javaspringtask1.javaspringtask1.model.TransactionStat;
-import jakarta.persistence.*;
-import org.hibernate.annotations.GenericGenerator;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
