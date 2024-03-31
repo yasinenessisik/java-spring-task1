329a36fb0cac9bf533f0cbe5e9623e8f41cb0a43
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-03-31 17:17:17 +0300

    Business, Service, Controller katmanları tamamlandı. Test kısmı ve Exceptionlar

--- src/main/java/com/javaspringtask1/javaspringtask1/dto/AccountCustomerDto.java
+++ src/main/java/com/javaspringtask1/javaspringtask1/dto/AccountCustomerDto.java
@@ -1,11 +1,5 @@
 package com.javaspringtask1.javaspringtask1.dto;
 
-import com.javaspringtask1.javaspringtask1.model.Account;
-import lombok.Builder;
-
-import java.math.BigDecimal;
-import java.util.Set;
-@Builder
 public class AccountCustomerDto {
     private String id;
     private String name;
